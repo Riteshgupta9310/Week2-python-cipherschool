2b5507a235f02c2b45baa17f395c991dbe2ff1dc
Riteshgupta9310 <115911203+Riteshgupta9310@users.noreply.github.com>
2023-01-11 23:17:48 +0530

    adding new file

---
+++ class2-strConcat-cipherSchool.py
@@ -0,0 +1,10 @@
+#collection of character inside single quotes or double quotes
+
+first_name = "Rahul"
+last_name = "Astuti"
+full_name = first_name +" "+ last_name
+print(full_name)
+#print(first_name+3).....error
+#print(first_name +"3")....no error
+#print(first_name + str(3))...no error
+print(full_name *3)
